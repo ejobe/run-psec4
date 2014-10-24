c07ceace566230572593bbf12f13fbc3a7ac6e85
eric oberla <eoberla@bitbucket.org>
2014-10-24 13:11:44 -0500

    Cygwin support for README

--- README.sh
+++ README.sh
@@ -3,16 +3,29 @@ echo ----------------------------------------------
 echo README
 echo updated 10/2014 ejo
 echo run-psec4 command-line functions and arguments
+echo Printed on $(uname -o) system on $(date)
 echo ----------------------------------------------
-./bin/refresh -h
-./bin/RawRead -h
-./bin/SetPed -h
-./bin/TakePed -h
-./bin/LinScan -h
-./bin/EnableTrig -h
-./bin/SetInternalTrig -h
-./bin/ScopeData -h
-./bin/LogData -h
+if [ $(uname -o) = 'Cygwin' ]; then
+    ./bin/refresh.exe -h
+    ./bin/RawRead.exe -h
+    ./bin/SetPed.exe -h
+    ./bin/TakePed.exe -h
+    ./bin/LinScan.exe -h
+    ./bin/EnableTrig.exe -h
+    ./bin/SetInternalTrig.exe -h
+    ./bin/ScopeData.exe -h
+    ./bin/LogData.exe -h
+else
+    ./bin/refresh -h
+    ./bin/RawRead -h
+    ./bin/SetPed -h
+    ./bin/TakePed -h
+    ./bin/LinScan -h
+    ./bin/EnableTrig -h
+    ./bin/SetInternalTrig -h
+    ./bin/ScopeData -h
+    ./bin/LogData -h     
+fi
 echo
 echo ----------------------------------------------
 
